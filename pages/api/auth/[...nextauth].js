32b9bf01fb13ba0f4a8308a0f739a36bba78a3bf
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2023-04-05 11:09:01 -0400

    Fake login data

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,24 +12,40 @@ export default NextAuth({
             },
             async authorize(credentials, req) {
 
-                const res = await fetch("your-backend-login-url", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
-                })
-                const user = await res.json()
+                // const res = await fetch("your-backend-login-url", {
+                //     method: 'POST',
+                //     body: JSON.stringify(credentials),
+                //     headers: { "Content-Type": "application/json" }
+                // })
+                // const user = await res.json()
 
-                if (res.ok && user) {
-                    return user
+                // if (res.ok && user) {
+                //     return user
+                // }
+
+                return {
+                    "mensaje": "Acceso concedido",
+                    "error": false,
+                    "data": {
+                        "id": 2205332,
+                        "nombre": "admin",
+                        "username": "admin",
+                        "idEmpresa": 2205317,
+                        "nombreEmpresa": "admin",
+                        "nombreRol": "Administrador",
+                        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkdhbmFkZXJhIiwiaWF0IjoxNjgwNzA2MTE3LCJleHAiOjE2ODA3MDk3MTd9.udDoKxi66eBZ5s4b2av1QZ_59OCa7PZbiMwXGlnDOO5qUMlE3i6L5MKQ0DOc44rE1hdLLwbpn2S44r25arPFdg",
+                        "permisions": [
+                            "Administrador"
+                        ],
+                        "password": ""
+                    }
                 }
-
-                return null
             }
         })
     ],
 
     callbacks: {
-        
+
         async jwt({ token, account, user }) {
 
             if (account) {
--- pages/login/index.js
+++ pages/login/index.js
@@ -8,8 +8,8 @@ import { useRouter } from "next/router"
 export default function LoginPage() {
 
     const router = useRouter()
-    const [username, setUsername] = useState('adminGanadera')
-    const [password, setPassword] = useState('ganacom2017')
+    const [username, setUsername] = useState('admin')
+    const [password, setPassword] = useState('admin')
 
 
     const login = async () => {
